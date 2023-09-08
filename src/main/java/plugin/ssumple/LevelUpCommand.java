52534982ab99065ea438d13eabf931d6d4671508
Yuki <12kaname111@gmail.com>
2023-09-08 23:07:14 +0900

    エンコーディングUTF-8にマップできません

---
+++ src/main/java/plugin/ssumple/LevelUpCommand.java
@@ -0,0 +1,17 @@
+package plugin.ssumple;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class LevelUpCommand implements CommandExecutor {
+
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if(sender instanceof Player player) {
+     player.setLevel(30);
+    }
+    return false;
+  }
+}
---
+++ src/main/java/plugin/ssumple/Main.java
@@ -0,0 +1,70 @@
+package plugin.ssumple;
+
+import java.util.Arrays;
+import java.util.Objects;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
+import org.bukkit.World;
+import org.bukkit.entity.Firework;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;A
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+import plugin.ssumple.LevelUpCommand;
+
+public final class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getCommand("levelup").setExecutor(new LevelUpCommand());
+    }
+
+    /**
+     * プレイヤーがスニークを開始/終了する際に起動されるイベントハンドラ。
+     *
+     * @param e イベント
+     */
+    @EventHandler
+    public void onPlayerToggleSneak(PlayerToggleSneakEvent e) {
+        // イベント発生時のプレイヤーやワールドなどの情報を変数に持つ。
+        Player player = e.getPlayer();
+        World world = player.getWorld();
+
+        // 花火オブジェクトをプレイヤーのロケーション地点に対して出現させる。
+        Firework firework = world.spawn(player.getLocation(), Firework.class);
+
+        // 花火オブジェクトが持つメタ情報を取得。
+        FireworkMeta fireworkMeta = firework.getFireworkMeta();
+
+        // メタ情報に対して設定を追加したり、値の上書きを行う。
+        // 今回は青色で星型の花火を打ち上げる。
+        fireworkMeta.addEffect(
+            FireworkEffect.builder()
+                .withColor(Color.BLUE)
+                .with(Type.STAR)
+                .withFlicker()
+                .build());
+        fireworkMeta.setPower(0);
+
+        // 追加した情報で再設定する。
+        firework.setFireworkMeta(fireworkMeta);
+    }
+}
+
+    @EventHandler
+    public void onPlayerBedEnter(PlayerBedEnterEvent e) {
+        Player player = e.getPlayer();
+        ItemStack[] itemStacks = player.getInventory().getContents();
+        Arrays.stream(itemStacks).filter(
+                item -> !Objects.isNull(item) && item.getMaxStackSize() == 64 && item.getAmount() < 30)
+            .forEach(item -> item.setAmount(64));
+        player.getInventory().setContents(itemStacks);
+    }
+}
