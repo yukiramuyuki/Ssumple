148204ab6c41ff607af01e1ea1d3e9a6441b9856
Yuki <12kaname111@gmail.com>
2023-10-06 22:30:25 +0900

    DAY11:設計　メモ

--- src/main/java/plugin/ssumple/Main.java
+++ src/main/java/plugin/ssumple/Main.java
@@ -15,7 +15,7 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
+import org.bukkit.event.Listener;/
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
@@ -24,6 +24,62 @@ import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin implements Listener {
+
+  /**
+   * 開発経験がないと設計は難しい
+   * 作ったことが無い料理はどう作るか
+   * 知識がない
+   *
+   * まず手を動かす
+   *
+   * filコマンド（しきつめる）
+   *
+   *
+   *
+   * マインクラフトプラグイン開発（タイトル）
+   *
+   * 機能要件・非機能要件・機能設計
+   * 
+   *
+   * 機能要件
+   * ログインできること
+   * 新規登録できること
+   * （ないとだめなもの）
+   * ゲームだと、
+   * 敵を倒すとてんすうが手に入ること
+   * 時間制限を設定できること
+   * 時間制限が来たら合計の点数が表示されること
+   * 　ゲームのルール。ないと成立しない
+   * それがないとだめ。携帯だと電話できないとだめ
+   * できてあたりまえ。できないとだめ
+   *
+   * 非機能要件
+   * 携帯だと軽い。必須ではないけど、あると嬉しい。
+   * 一番重要
+   * 顧客満足度。お客さんが求めているもの。
+   * いくらでもだせる。
+   * コマンドでゲームを開始できる。
+   * コマンドのオプションで敵の強さ、プライヤーの強さ、敵の種類を設定できる。
+   * プラグインをどのサーバーでも動かせること。
+   * 同時に。
+   * 非機能を満たすために機能をある程度考慮していないと。
+   *
+   * 機能設計
+   * 点が入る敵を倒すと。要件定理。何を使うか。
+   * 敵を倒すと点数が手に入る
+   * 　EntityのSpownの仕組みを使って敵を出現させる。
+   * 　Entityが倒れた時のイベントを使って、（スコアとしない。）点数を設定する。
+   *
+   * スコアを点数を変える。スコア、点数同じ意味でも統一させる。認識が違うこともあるから。
+   * 敵をモンスター。敵イコールモンスターの認識ではないかも。
+   * 点数とは何かも設計。
+   *
+   * 簡単なメモでもいい。まとめておいて残しておく
+   * きちんとメンテナンスをしておくと、後で見返したときにプラグインの確認などで資料としてのこっているため
+   * 思い返しやすい。
+   * 抜いたり、足したり自由に。（お客さんとするときは勝手に抜いたりはダメ）
+   */
+
 
     @Override
     public void onEnable() {
